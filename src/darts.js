1dbbb957ff40aef0369b744aeb4e0fdeeb89c2d8
Marek Jędrzejewski <marekjedrzejewski@github.com>
2015-05-09 13:48:04 +0200

    Oh, so I have to also add modified files when I push?

--- src/darts.js
+++ src/darts.js
@@ -1,1 +1,27 @@
-//test
+function drawingTest() {
+	if (SVG.supported) {
+		var scale = window.innerWidth*0.5;
+		var draw = SVG('drawing').size(scale,scale);
+
+		var list_of_objects = [];
+		for (var i = 0; i < 20; i++) {
+			// list_of_objects[i] = new SVG.Rect();
+			list_of_objects[i] = draw.rect(0.08*scale, 0.08*scale);
+			list_of_objects[i].move(0.5*scale,0.3*scale);
+			list_of_objects[i].mouseover(lightup);
+			list_of_objects[i].mouseout(lightdown);
+			list_of_objects[i].rotate(18*i, 0.5*scale, 0.5*scale);
+		}
+	}
+	else {
+		alert('SVG not supported');
+	}
+}
+
+var lightup = function() {
+	this.fill("#fc0");
+};
+
+var lightdown = function() {
+	this.fill('#000');
+};
