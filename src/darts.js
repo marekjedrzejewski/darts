2a886f4a8077fe1ec214519138775dee65599515
Marek Jędrzejewski <marekjedrzejewski@github.com>
2015-05-09 14:16:11 +0200

    So there's 'commit -am'. Nice. Also: OMGLOOK, Aperture logo!

--- src/darts.js
+++ src/darts.js
@@ -5,8 +5,7 @@ function drawingTest() {
 
 		var list_of_objects = [];
 		for (var i = 0; i < 20; i++) {
-			// list_of_objects[i] = new SVG.Rect();
-			list_of_objects[i] = draw.rect(0.08*scale, 0.08*scale);
+			list_of_objects[i] = draw.polygon('1,1 30,30 20,1');
 			list_of_objects[i].move(0.5*scale,0.3*scale);
 			list_of_objects[i].mouseover(lightup);
 			list_of_objects[i].mouseout(lightdown);
