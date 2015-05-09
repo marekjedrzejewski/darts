3e6e68fcba5f4d4af4ff8d6f3c0e8129f4239747
marekjedrzejewski <marekjedrzejewski@github.com>
2015-05-09 22:21:02 +0200

    Cleaning code

--- src/darts.js
+++ src/darts.js
@@ -1,37 +1,21 @@
 function drawingTest() {
 	if (SVG.supported) {
-		var scale = window.innerHeight*0.8;
-		var segment_rotation = 18;
-		var draw = SVG('drawing').size(scale,scale);
 
-		var outerboard = draw.circle(scale,scale);
-		outerboard.fill('#1b0d24');
+		var draw = SVG('drawing').size(n.scale,n.scale);
+
+		var outerboard = draw.circle(n.scale,n.scale);
+		outerboard.fill(c.outerboard);
 		outerboard.mouseover(lightup);
 		outerboard.mouseout(lightdown);
-
-		// Shame I don't know math and what is sin/cos :(
-		// But I can use Inkscape <3
-		// point: up 41292, left 06563
-		var main_segment_points = 'M' +
-															Math.round(scale/2) + ',' + Math.round(scale/2) +
-															' L' +
-															Math.round(scale/2 - 0.06563*scale) + ',' +
-															Math.round(scale/2 - 0.41292*scale) + ' ' +
-															'A180,180 0 0,1 ' +
-															Math.round(scale/2 + 0.06563*scale) + ',' +
-															Math.round(scale/2 - 0.41292*scale);
-
-
-		console.log(main_segment_points);
 
 		var list_of_objects = [];
 
 		// Generating main segments of the board
-		for (var i = 0; i < 20; i++) {
-			list_of_objects[i] = draw.path(main_segment_points);
+		for (var i = 0; i < n.segments; i++) {
+			list_of_objects[i] = draw.path(main_segment_shape);
 			list_of_objects[i].mouseover(lightup);
 			list_of_objects[i].mouseout(lightdown);
-			list_of_objects[i].rotate(segment_rotation*i, 0.5*scale, 0.5*scale);
+			list_of_objects[i].rotate(n.segment_rotation*i, n.center, n.center);
 			list_of_objects[i].data({value: null});
 			list_of_objects[i].fill(color_main_segments(i));
 			}
@@ -41,6 +25,56 @@ function drawingTest() {
 		alert('SVG not supported');
 	}
 }
+
+
+// Converts from degrees to radians.
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+
+
+// n is for numbers
+var n = {
+	scale: window.innerHeight*0.8,
+	segments: 20,
+	segment_rotation: 18
+};
+// I can't do that inside :( Why?
+n.center = n.scale/2;
+n.main_len = n.scale*0.42;
+
+// c is for colors
+var c = {
+	outerboard: "#1b0d24",
+	main_segment_odd: "#f0ebd6",
+	main_segment_even: "#1a2119"
+};
+
+var main_segment_shape =
+		'M' + // start on center
+		Math.round(n.center) + ',' + Math.round(n.center) +
+		' L' + // link to left point
+		Math.round(
+			n.center + n.main_len *
+			Math.cos(Math.radians(270-n.segment_rotation/2))
+		) + ',' +
+		Math.round(
+			n.center + n.main_len *
+			Math.sin(Math.radians(270-n.segment_rotation/2))
+		) + ' ' +
+		'A' + // arc to
+		Math.round(n.main_len) + ',' + Math.round(n.main_len) +
+		' 0 0,1 ' +
+		// right point
+		Math.round(
+			n.center + n.main_len *
+			Math.cos(Math.radians(270+n.segment_rotation/2))
+		) + ',' +
+		Math.round(
+			n.center + n.main_len *
+			Math.sin(Math.radians(270+n.segment_rotation/2))
+		);
+
 
 var lightup = function() {
 	this.filter(function(add) {
@@ -60,9 +94,9 @@ var lightdown = function() {
 
 function color_main_segments(i) {
 	if (i % 2 === 0) {
-		return "#1a2119";
+		return c.main_segment_even;
 	}
 	else {
-		return "#f0ebd6";
+		return c.main_segment_odd;
 	}
 }
